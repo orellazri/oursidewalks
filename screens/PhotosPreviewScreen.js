0a9c5a31022d40f4f06a60e2efb56c51655643e4
Orel <ryiseld@gmail.com>
2022-02-25 20:32:55 +0200

    Add photos preview screen

--- screens/PhotosPreviewScreen.js
+++ screens/PhotosPreviewScreen.js
@@ -1,10 +1,42 @@
-import { View, Text } from "react-native";
-import React from "react";
+import { View, Text, SafeAreaView, StyleSheet, TouchableOpacity } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
-export default function PhotosPreviewScreen() {
+export default function PhotosPreviewScreen({ navigation }) {
+  const handleClose = () => {
+    navigation.navigate("PromptCapture");
+  };
+
   return (
-    <View>
-      <Text>PhotosPreviewScreen</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      {/* Title */}
+      <View style={styles.titleContainer}>
+        <TouchableOpacity style={styles.closeButton} onPress={handleClose}>
+          <AntDesign name="close" size={26} color="black" />
+        </TouchableOpacity>
+        <Text style={styles.title}>בחירת תמונות</Text>
+        {/* Placeholder to align title to center */}
+        <View></View>
+      </View>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    alignItems: "center",
+  },
+  titleContainer: {
+    marginTop: 30,
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+  },
+  title: {
+    fontFamily: "Assistant-Bold",
+    fontSize: 18,
+    marginRight: 30,
+  },
+  closeButton: {
+    marginLeft: 30,
+  },
+});
