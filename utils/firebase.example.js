e8d8dd10a20222cc2fae50d04174514788718344
avia-haddad <aviahadd@gmail.com>
2022-03-21 17:00:26 +0200

    Add experimentalForceLongPolling to Firestore

--- utils/firebase.example.js
+++ utils/firebase.example.js
@@ -1,7 +1,7 @@
 import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+import { initializeFirestore } from "firebase/firestore";
 
 const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
-export const db = getFirestore();
+export const db = initializeFirestore(app, { experimentalForceLongPolling: true });
