eac4cf5f07e1765fd1c805ff1e16f9aaca016b92
orellazri <ryiseld@gmail.com>
2022-04-09 19:44:58 +0300

    Add firebase storage

--- utils/firebase.example.js
+++ utils/firebase.example.js
@@ -1,7 +1,9 @@
 import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 export const db = initializeFirestore(app, { experimentalForceLongPolling: true });
+export const storage = getStorage(app);
