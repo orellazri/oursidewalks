4df3cf1f3f9e7ac92a400067f647b3c45b462e94
orellazri <ryiseld@gmail.com>
2022-04-30 13:42:07 +0300

    Add auth to firebase example

--- utils/firebase.example.js
+++ utils/firebase.example.js
@@ -1,9 +1,11 @@
 import { initializeApp } from "firebase/app";
 import { initializeFirestore } from "firebase/firestore";
 import { getStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {};
 
 const app = initializeApp(firebaseConfig);
 export const db = initializeFirestore(app, { experimentalForceLongPolling: true });
 export const storage = getStorage(app);
+export const auth = getAuth(app);
