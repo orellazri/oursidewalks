0839b0ecf3735d923ffa733a87965d542276cfa9
orellazri <ryiseld@gmail.com>
2022-05-06 10:42:36 +0300

    Remove welcome screen snapshot test

--- app/__tests__/screens/Welcome.test.js
+++
@@ -1,8 +0,0 @@
-import renderer from "react-test-renderer";
-
-import WelcomeScreen from "screens/WelcomeScreen";
-
-it("renders correctly", () => {
-  const tree = renderer.create(<WelcomeScreen />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
