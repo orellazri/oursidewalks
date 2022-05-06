60358383ae23a2049b6b0ca66f19f53018d94276
orellazri <ryiseld@gmail.com>
2022-05-06 14:34:29 +0300

    Get document in report context

--- app/utils/ReportContext.js
+++ app/utils/ReportContext.js
@@ -1,10 +1,14 @@
 import { createContext, useState, useEffect, useContext } from "react";
 import { getItemAsync } from "expo-secure-store";
+import { doc, getDoc } from "firebase/firestore";
+
+import { db } from "../utils/firebase";
 
 const ReportContext = createContext(null);
 
 export const ReportProvider = ({ children }) => {
   const [uid, setUid] = useState("");
+  const [user, setUser] = useState({});
   const [photos, setPhotos] = useState([]);
   const [hazardType, setHazardType] = useState("");
   const [freetext, setFreetext] = useState("");
@@ -29,9 +33,20 @@ export const ReportProvider = ({ children }) => {
   // Retrieve user from persistent storage on start
   useEffect(async () => {
     const result = await getItemAsync("uid");
-    if (result != "") {
+    if (result && result != "") {
       // User is stored
-      // TODO: validate uid and get details and save to state. if doesn't exist in firestore, remove key from storage
+
+      // Validate uid and get details
+      const docSnapshot = await getDoc(doc(db, "users", result));
+      if (!docSnapshot.exists()) {
+        // UID doesn't exist. Remove uid from persistent storage and state
+        setUid("");
+        await setItemAsync("uid", "");
+        return;
+      }
+
+      // UID exists. TODO: Get data
+      console.log(docSnapshot.data());
       setUid(result);
     }
   }, []);
