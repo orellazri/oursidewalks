654218221260d5c17bb8d4b69d0f8aa09049c89d
orellazri <ryiseld@gmail.com>
2022-07-05 11:09:56 +0300

    Increment build number

--- app/app.config.js
+++ app/app.config.js
@@ -18,7 +18,7 @@ export default {
   assetBundlePatterns: ["**/*"],
   ios: {
     bundleIdentifier: "org.ourstreets.ourstreets",
-    buildNumber: "4",
+    buildNumber: "5",
     supportsTablet: false,
     config: {
       googleMapsApiKey: process.env.IOS_MAPS_API_KEY,
@@ -31,7 +31,7 @@ export default {
   },
   android: {
     package: "org.ourstreets.ourstreets",
-    versionCode: 4,
+    versionCode: 5,
     config: {
       googleMaps: {
         apiKey: process.env.ANDROID_MAPS_API_KEY,
